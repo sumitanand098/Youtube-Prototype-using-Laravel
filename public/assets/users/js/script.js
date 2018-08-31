9a7658530d4f8c80c1cb6d08cf8efdf7b4588a5e
Sumit Anand <37323280+sumitanand098@users.noreply.github.com>
2018-09-01 01:37:32 +0530

    Add files via upload

---
+++ public/assets/users/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	// console.log("hi");
+ //    $('.input').focus(function(){
+ //    	var tab_id = $(this).attr('href');
+ //        $(tab_id).addClass('current')
+ //    }).blur(function(){
+ //        var tab_id = $(this).attr('href');
+ //        $(tab_id).removeClass('current')
+ //    });
+    $(".slidebar-icon").click(function(){
+	    $("#full-view").toggleClass("width100");
+	    $("#full-view").toggleClass("left-right-padding");
+	    $("#side-view").toggleClass("zero-width");
+	});
+	$('.tab-link').click(function(){
+		var tab_id = $(this).attr('href');
+		console.log(tab_id);
+		$('.tab-link').removeClass('current');
+		$(this).addClass('current');
+	});
+});
